387f32bd06b6f5be42d20ff6361fd3e85d4b8abd
MattCo87 <77285498+MattCo87@users.noreply.github.com>
2022-03-22 17:17:51 +0100

    inscriptionOK

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -33,6 +33,8 @@ class RegistrationController extends AbstractController
                     $form->get('plainPassword')->getData()
                 )
             );
+            $user->setExperience(1);
+            $user->setRoles(['ROLE_USER']);
 
             $entityManager->persist($user);
             $entityManager->flush();
